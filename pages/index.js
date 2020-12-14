c754522d116ea3b4b107c43a395b24d194fb34fc
Salman Zafar <salman.zafar@innowi.com>
2020-12-14 16:18:22 +0500

    job batching laravel front end

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+export default function index(){
+
+  return <div className="flex h-screen ">
+    <div className="m-auto">
+      <h1>
+        Hello
+      </h1>
+    </div>
+  </div>
+}
---
+++ pages/upload.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Link from "next/link";
+
+export default function upload(){
+
+    return (
+        <div>
+            <Link href='/'>Home</Link>
+            <Link href='/upload'>Upload</Link>
+        </div>
+    )
+}
