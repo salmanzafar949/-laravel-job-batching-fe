532bcab95072734214f6116b516129f2657eb061
Salman Zafar <salman.zafar@innowi.com>
2020-12-15 15:02:01 +0500

    working on batch detail almost done

--- pages/upload.js
+++ pages/upload.js
@@ -1,9 +1,11 @@
-import React, {useRef} from 'react';
+import React, {useRef, useEffect, useState} from 'react';
 import Layout from "../components/Layout";
 
 export default function upload(){
 
     const fileRef = useRef();
+    const API_URL = "http://127.0.0.1:8000/api/";
+    const [batchInfo, setBatchInfo] = useState({});
 
     function handleForm(e)
     {
@@ -11,8 +13,27 @@ export default function upload(){
         const inputFile = fileRef.current;
         const file = inputFile.files[0];
         if (!file) return;
-        console.log(file);
+
+        const fd = new FormData();
+        fd.append('file', file)
+
+        fetch(`${API_URL}upload`, {method: "post", body: fd})
+            .then(res => res.json())
+            .then(data => setBatchInfo(data))
     }
+
+    function batchDetails()
+    {
+        const batchId = "923fc877-9818-487c-a8e0-0624e87dc75a";
+
+        fetch(`${API_URL}batch?id=${batchId}`)
+            .then(res => res.json())
+            .then(data => console.log(data))
+    }
+
+    useEffect(() => {
+        batchDetails();
+    },[]);
 
     return (
         <Layout>
